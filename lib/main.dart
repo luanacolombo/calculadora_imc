2c95748a407b4e1abcfc4efd556a47c49aaafd67
luanacolombo <luuana.colombo@gmail.com>
2023-11-05 15:04:40 -0300

    Controller e Cálculos

--- lib/main.dart
+++ lib/main.dart
@@ -12,48 +12,90 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+//_ na frente, significa que é privada
 class _HomeState extends State<Home> {
+  //controlador do peso
+  TextEditingController weightController = TextEditingController();
+  //controlador da altura
+  TextEditingController heightController = TextEditingController();
+
+  String _infoText = "Informe seus dados!"; //modifica o texto
+
+  void _resetFields() {
+    weightController.text = ""; //diz que o texto do controlador é vazio
+    heightController.text = ""; //diz que o texto do controlador é vazio
+    _infoText = "Informe seus dados!";
+  }
+
+  void _calculate() {
+    setState(() {
+      //pega o texto e transforma em double
+      double weight = double.parse(weightController.text);
+      double height =
+          double.parse(heightController.text) / 100; //p/ transformar em metros
+      //calcula o imc
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Calculadora de IMC"),
+          title: const Text("Calculadora de IMC"),
           centerTitle: true, //centraliza o texto
           backgroundColor: Colors.green, //cor da appBar
           actions: [
             IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: () {},
+              icon: const Icon(Icons.refresh),
+              onPressed: _resetFields,
             )
           ],
         ),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.white, //cor do fundo
+        //corpo do app
         body: SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment
                 .stretch, //stretch tenta alargar tudo que tem na coluna
             children: [
-              Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+              const Icon(Icons.person_outline,
+                  size: 120.0, color: Colors.green),
               TextField(
                 keyboardType: TextInputType.number,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                     labelText: "Peso (kg)",
-                    labelStyle: TextStyle(
-                        color: const Color.fromARGB(235, 76, 175, 79))),
+                    labelStyle:
+                        TextStyle(color: Color.fromARGB(235, 76, 175, 79))),
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                style: const TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: weightController,
               ),
               TextField(
                 keyboardType: TextInputType.number,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                     labelText: "Altura (cm)",
                     labelStyle: TextStyle(color: Colors.green)),
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                style: const TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: heightController,
               ),
               Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
                 child: Container(
                   height: 50.0,
                   child: ElevatedButton(
@@ -65,12 +107,12 @@ class _HomeState extends State<Home> {
                       "Calcular",
                       style: TextStyle(fontSize: 25.0),
                     ),
-                    onPressed: () {},
+                    onPressed: _calculate,
                   ),
                 ),
               ),
               Text(
-                "Info",
+                _infoText,
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.green, fontSize: 25.0),
               )
