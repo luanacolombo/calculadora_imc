8b4ab74aee8d75a5a9d936ad60e6a7efb428fa75
luanacolombo <luuana.colombo@gmail.com>
2023-11-05 14:16:17 -0300

    ScrollViem e ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -16,41 +16,66 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        centerTitle: true, //centraliza o texto
-        backgroundColor: Colors.green, //cor da appBar
-        actions: [
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment
-            .stretch, //stretch tenta alargar tudo que tem na coluna
-        children: [
-          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Peso (kg)",
-                labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true, //centraliza o texto
+          backgroundColor: Colors.green, //cor da appBar
+          actions: [
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment
+                .stretch, //stretch tenta alargar tudo que tem na coluna
+            children: [
+              Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(
+                        color: const Color.fromARGB(235, 76, 175, 79))),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll<Color>(Colors.green),
+                    ),
+                    child: const Text(
+                      "Calcular",
+                      style: TextStyle(fontSize: 25.0),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Text(
+                "Info",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              )
+            ],
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Altura (cm)",
-                labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
